9f5114f94ece3b4b5a6cbc8b07a3acbe3af35a0a
“balmes2baxter” <balmescamilobritovargas@gmail.com>
2025-09-03 13:32:35 -0500

    feat: backend funcional con JWT y endpoints protegidos

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.routers import DefaultRouter
+from core.views import ClienteViewSet
+
+router = DefaultRouter()
+router.register(r"clientes", ClienteViewSet, basename="cliente")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # 🔐 Rutas de autenticación con JWT
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/", include(router.urls)),
 ]
